3ff3874f09381d552e527cac25056a05b396e7c0
Eduardo de la Rosa <81875548+Eduardo-dlrf@users.noreply.github.com>
2026-06-18 23:05:21 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,10 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /urs/bin/portfolio.py
+python3 /usr/bin/portfolio.py
 
-git add -A && git commit -m "Update Portfolio"
+git add -A 
+git commit -m "Update Portfolio XML via Docker" || echo "No changes to commit"
 git push --set-upstream origin main
 
 echo "=================="
